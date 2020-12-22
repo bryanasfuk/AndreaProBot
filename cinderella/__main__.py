fb7e3d4bcd3f636fd3a9c74ed326ec4a9662b260
Saravanakrish <68471536+saravanakrish97@users.noreply.github.com>
2020-12-22 20:58:17 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -41,7 +41,6 @@ the things I can help you with.
  💠 - /start: start the bot
  💠 - /help: PM's you this message.
  💠 - /help <module name>: PM's you info about that module.
- 💠 - /source: Information about my source.
  💠 - /settings:
    🔹 - in PM: will send you your settings for all supported modules.
    🔹 - in a group: will redirect you to pm, with all that chat's settings.
@@ -179,7 +178,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="Help❓ ",callback_data="help_back"),InlineKeyboardButton(text="Creator 🔱",url="https://t.me/SARAVANAKRISH")]]
+    keyboard = [[InlineKeyboardButton(text="Help❓ ",callback_data="help_back"),InlineKeyboardButton(text="Creator 🔱",url="https://t.me/TAMILTALKING")]]
     keyboard += [[InlineKeyboardButton(text="Connect Group 🔄", callback_data="main_connect"),InlineKeyboardButton(text="Add Me 🔆",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
@@ -281,7 +280,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands💃.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="Help❓",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🚸 Contact Creator 🚸",url="https://t.me/SARAVANAKRISH")]]))
+                                                [InlineKeyboardButton(text="🚸 Contact Creator 🚸",url="https://t.me/TAMILTALKING")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
